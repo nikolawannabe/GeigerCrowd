c26803a1aeb59d6629bef105c3ab4f0590afc3ba
nikolawannabe <sharon.talbot@gmail.com>
2011-03-16 23:08:28 -0700

    add more stuff

---
+++ GeigerCrowd/Scripts/api/api.readingpoints.js
@@ -0,0 +1,13 @@
+﻿var readingPointsApi = function () { }
+
+readingPointsApi.prototype.getAllReadings = function (options) {
+    var config = $.extend({
+        success: function () { },
+        error: function () { }
+    }, options);
+
+    $.apiCall({
+        url: '/api/getAllReadings',
+        success: function (result) { config.success(result); }
+    });
+}
